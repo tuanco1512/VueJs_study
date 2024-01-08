a746fa1d998b81d8d371d80502b4077a7eba6024
tuanco1512 <tuanco15122000@gmail.com>
2024-01-08 10:29:29 +0700

    no message

--- NuxtJs/components/desktop/inputForm.vue
+++ NuxtJs/components/desktop/inputForm.vue
@@ -1,13 +1,4 @@
 <template>
-<<<<<<< HEAD
-  <div>
-    <div v-for="(item, index) in conssignmentArray" :key="index">
-      <div>{{ item.full_address }}</div>
-      <div>{{ item.price }}</div>
-    </div>
-    <hr />
-    <button @click.prevent="loadData" v-if="isLoadData">Load Data</button>
-=======
   <div >
     <div v-for="(item, index) in consignmentArray" :key="index">
       <NuxtLink :to="`/detail/${item.post_link}`">{{ item.full_address }}</NuxtLink>
@@ -17,56 +8,20 @@
     <button @click.prevent="loadData" v-if="isLoadData">
       Load Data
     </button>
->>>>>>> CallAPI
   </div>
 </template>
 <script>
 export default {
   data() {
     return {
-<<<<<<< HEAD
-      conssignmentArray: [],
-      page: 1,
-      isLoadData: true,
-=======
       consignmentArray:[],
       page:1,
       isLoadData:true
->>>>>>> CallAPI
     };
   },
 
   mounted() {
-<<<<<<< HEAD
-    this.loadData();
-  },
-
-  methods: {
-    async loadData() {
-      try {
-        if (this.isLoadData) {
-          const response = await this.$axios.get(
-            `/get-consignment-list?page=${this.page}`
-          );
-
-          //this.conssignmentArray = response.data.filterResult.data
-          this.conssignmentArray = [
-            ...this.conssignmentArray,
-            ...response.data.filterResult.data,
-          ];
-
-          this.page++;
-          if (response.data.filterResult.data.length === 0) {
-            this.isLoadData = false;
-          }
-        }
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    },
-=======
     this.loadData()
->>>>>>> CallAPI
   },
 
   methods: {
